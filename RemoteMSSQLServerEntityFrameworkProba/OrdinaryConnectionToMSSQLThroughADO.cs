dc9e509a43861acaec72fe66ea2e5cd2d1eeeef8
sergiosssg <sergiosssg@gmail.com>
2022-09-12 15:44:54 +0300

    in class  OrdinaryConnectionToMSSQLThroughADO continue  edditing static method AccomplishSelectTo_TEL_VID_CONNECT.

--- RemoteMSSQLServerEntityFrameworkProba/OrdinaryConnectionToMSSQLThroughADO.cs
+++ RemoteMSSQLServerEntityFrameworkProba/OrdinaryConnectionToMSSQLThroughADO.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,48 @@ namespace RemoteMSSQLServerEntityFrameworkProba
     {
         static public void AccomplishSelectTo_TEL_VID_CONNECT()
         {
+            Console.WriteLine("Start program");
 
+
+            using (SqlConnection sqlCon = new SqlConnection(DBFacilities.GetConnectionString()))
+            {
+                string sqlCmdExTableCheck = @"SELECT *  FROM [SM].[phone].[TEL_VID_CONNECT]";
+
+                using (SqlCommand sqlCmd = new SqlCommand(sqlCmdExTableCheck, sqlCon))
+                {
+                    try
+                    {
+                        ConnectionState conState = sqlCon.State;
+
+                        Console.WriteLine(conState);
+
+                        sqlCon.Open();
+
+                        Console.WriteLine("connection open successfully");
+
+                        conState = sqlCon.State;
+
+                        Console.WriteLine(conState);
+
+                        using (SqlDataReader reader = sqlCmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine(String.Format("{0}, {1}, {2}",
+                                    reader[0], reader[1], reader[2]));
+                            }
+                        }
+                        sqlCon.Close();
+                        Console.WriteLine("connection closed successfully");
+                    }
+                    catch (SqlException se)
+                    {
+                        ;
+                    }
+
+                    Console.WriteLine("end of program");
+                }
+            }
         }
-
-
-
     }
 }
