2b785c03a5164276dfdefc5078e14d36c78fba3e
sergiosssg <sergiosssg@gmail.com>
2022-09-12 18:11:33 +0300

    in class  WorkingWithEntitiesFromMSSQLThroughEF continue  edditing static method AccomplishSelectTo_TEL_VID_CONNECT.

--- RemoteMSSQLServerEntityFrameworkProba/WorkingWithEntitiesFromMSSQLThroughEF.cs
+++ RemoteMSSQLServerEntityFrameworkProba/WorkingWithEntitiesFromMSSQLThroughEF.cs
@@ -15,6 +15,18 @@ namespace RemoteMSSQLServerEntityFrameworkProba
             DbContextOptions<SimpleDataModel> OptionsOFContext = new DbContextOptionsBuilder<SimpleDataModel>().UseSqlServer(  DBFacilities.GetConnectionString()).Options;       /// DataBaseFacilities.OptionsOfDbContext();
             SimpleDataModel dataModel = new SimpleDataModel(OptionsOFContext);
 
+            dataModel.pO_TEL_VID_CONNECTs.Load();
+
+            var colOfPO = dataModel.pO_TEL_VID_CONNECTs.ToList<PO_TEL_VID_CONNECT>();
+
+            Console.WriteLine(" collection is read");
+
+            foreach (var elOfPO in colOfPO)
+            {
+                Console.WriteLine(String.Format("{0}, {1}, {2}", elOfPO.IDConnect, elOfPO.KodOfConnect, elOfPO.NameOfConnect));
+
+            }
+            Console.WriteLine(" collection is printed");
         }
 
     }
