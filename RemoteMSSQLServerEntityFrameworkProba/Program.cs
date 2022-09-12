056fdab66bbbf8e460b221aa7f1703ffbec2c938
sergiosssg <sergiosssg@gmail.com>
2022-09-12 15:58:22 +0300

    in  Program.cs  added  static method  Main :   --  static void Main(string[] args) .

--- RemoteMSSQLServerEntityFrameworkProba/Program.cs
+++ RemoteMSSQLServerEntityFrameworkProba/Program.cs
@@ -1,60 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
-
-using RemoteMSSQLServerEntityFrameworkProba;
-using System.Data;
-using System.Data.SqlClient;
-
-Console.WriteLine("Start program");
-
-
-using (SqlConnection sqlCon = new SqlConnection(DBFacilities.GetConnectionString()))
+static void Main(string[] args)
 {
-    //bool checkTable;
-    //string sqlCmdExTableCheck = @"SELECT *  FROM [sampd_cexs].[dbo].[TEL_VID_CONNECT]";   // for table in local MS SQL Server
-    string sqlCmdExTableCheck = @"SELECT *  FROM [SM].[phone].[TEL_VID_CONNECT]";  // for table in remote MS SQL Server
-
-    using (SqlCommand sqlCmd = new SqlCommand(sqlCmdExTableCheck, sqlCon))
-    {
-        try
-        {
-
-            ConnectionState conState = sqlCon.State;
-
-            Console.WriteLine(conState);
-
-            sqlCon.Open();
-
-            Console.WriteLine("connection open successfully");
-
-            conState = sqlCon.State;
-
-            Console.WriteLine(conState);
-
-            using (SqlDataReader reader = sqlCmd.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    Console.WriteLine(String.Format("{0}, {1}, {2}",
-                        reader[0], reader[1], reader[2]));
-                }
-            }
-            sqlCon.Close();
-            Console.WriteLine("connection closed successfully");
-        }
-        catch (SqlException se)
-        {
-            ;
-        }
-    }
-    Console.WriteLine("end of program");
-
-
-/*
-    DbContextOptions<DbAppContext> OptionsOFContext = DataBaseFacilities.OptionsOfDbContext();
-    DbAppContext = new DbAppContext(OptionsOFContext);
-*/
+    ;
 
 }
+
+
 
