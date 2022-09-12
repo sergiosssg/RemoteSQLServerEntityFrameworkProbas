8c68c962181b988e42852377af54a4e4ae2258a1
sergiosssg <sergiosssg@gmail.com>
2022-09-12 13:06:50 +0300

    Добавьте файлы проекта.

---
+++ RemoteMSSQLServerEntityFrameworkProba/DBFacilities.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteMSSQLServerEntityFrameworkProba
+{
+    public partial class DBFacilities
+    {
+        public static string GetConnectionString()
+        {
+            DbConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder["Data Source"] = @"xplandev\DEVMAIN";         //  Office setting string for development  MS SQL DB in interprise server
+            //builder["Data Source"] = "localhost";               //  Office setting string for local MS SQL DB
+            //builder["Data Source"] = @"localhost\SQLExpress";   //  Home setting string for local MS SQL DB
+
+
+            builder["Database"] = "SM";                            //  Office setting string for development  MS SQL DB in interprise server
+            //builder["Database"] = "sampd_cexs";                  //  Office and Home setting string for local MS SQL DB
+
+            builder["integrated Security"] = "true";
+
+            return builder.ConnectionString;
+        }
+    }
+}
---
+++ RemoteMSSQLServerEntityFrameworkProba/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Microsoft.Data.SqlClient;
+using RemoteMSSQLServerEntityFrameworkProba;
+using System.Data;
+
+Console.WriteLine("Start program");
+
+
+using (SqlConnection sqlCon = new SqlConnection(DBFacilities.GetConnectionString()))
+{
+    //bool checkTable;
+    //string sqlCmdExTableCheck = @"SELECT *  FROM [sampd_cexs].[dbo].[TEL_VID_CONNECT]";   // for table in local MS SQL Server
+    string sqlCmdExTableCheck = @"SELECT *  FROM [SM].[phone].[TEL_VID_CONNECT]";  // for table in remote MS SQL Server
+
+    using (SqlCommand sqlCmd = new SqlCommand(sqlCmdExTableCheck, sqlCon))
+    {
+        try
+        {
+
+            ConnectionState conState = sqlCon.State;
+
+            Console.WriteLine(conState);
+
+            sqlCon.Open();
+
+            Console.WriteLine("connection open successfully");
+
+            conState = sqlCon.State;
+
+            Console.WriteLine(conState);
+
+            using (SqlDataReader reader = sqlCmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}, {1}, {2}",
+                        reader[0], reader[1], reader[2]));
+                }
+            }
+            sqlCon.Close();
+            Console.WriteLine("connection closed successfully");
+        }
+        catch (SqlException se)
+        {
+            ;
+        }
+    }
+    Console.WriteLine("end of program");
+}
+
