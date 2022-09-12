59e37dd356d373d6671a46a3fff2cbb08745eabc
sergiosssg <sergiosssg@gmail.com>
2022-09-12 18:13:51 +0300

    in Main call  WorkingWithEntitiesFromMSSQLThroughEF.AccomplishSelectTo_TEL_VID_CONNECT(); .

--- RemoteMSSQLServerEntityFrameworkProba/Program.cs
+++ RemoteMSSQLServerEntityFrameworkProba/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using RemoteMSSQLServerEntityFrameworkProba;
+
 static void Main(string[] args)
 {
-    ;
+    WorkingWithEntitiesFromMSSQLThroughEF.AccomplishSelectTo_TEL_VID_CONNECT();
 
 }
 
