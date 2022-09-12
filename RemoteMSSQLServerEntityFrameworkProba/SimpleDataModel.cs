83c10f6341b3d5beb2b61c0303f4786a792ef003
sergiosssg <sergiosssg@gmail.com>
2022-09-12 15:17:23 +0300

    crated  data model  class  SimpleDataModel  :    --  public partial  class SimpleDataModel : DbContext    with  public property :    --  public DbSet<PO_TEL_VID_CONNECT>? pO_TEL_VID_CONNECTs { get; set; } = null;    and with overrided  method  OnModelCreating  :    --   protected override void OnModelCreating(ModelBuilder modelBuilder) .

---
+++ RemoteMSSQLServerEntityFrameworkProba/SimpleDataModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteMSSQLServerEntityFrameworkProba
+{
+    public partial  class SimpleDataModel : DbContext
+    {
+
+        public SimpleDataModel( DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<PO_TEL_VID_CONNECT>? pO_TEL_VID_CONNECTs { get; set; } = null;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("pnone");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
